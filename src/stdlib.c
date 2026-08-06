1623218b6417109830d10bdd5ca55ca757febf9b
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 13:27:27 -0700

    Enforce semantic wrapper boundaries

--- src/stdlib.c
+++ src/stdlib.c
@@ -14,16 +14,7 @@
  * limitations under the License.
  */
 
-#include "p101_filesystem/p101_dirent.h"
-#include "p101_filesystem/p101_fnmatch.h"
-#include "p101_filesystem/p101_ftw.h"
-#include "p101_filesystem/p101_glob.h"
-#include "p101_filesystem/p101_libgen.h"
-#include "p101_filesystem/p101_stdio.h"
 #include "p101_filesystem/p101_stdlib.h"
-#include "p101_filesystem/p101_unistd.h"
-#include "p101_filesystem/sys/p101_stat.h"
-#include "p101_filesystem/sys/p101_statvfs.h"
 #include <p101_env/wrapper.h>
 
 /*
